6b2b9941aeee34f741c5e76b3f83183766c0fa21
Gonçalo Malheiro <49911025+GoncaloMalheiro@users.noreply.github.com>
2020-03-09 17:56:35 +0000

    Add files via upload

---
+++ teste/Contador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste
+{
+    class Contador
+    {
+        private string _path = Application.StartupPath + "\\Resources.Contador.txt";
+            
+
+        public void contar(object sender)
+        {
+            Button b = (Button)sender;
+            int valor = Convert.ToInt32(b.Text) + 1;
+            b.Text = valor.ToString();
+            File.WriteAllText(_path, valor.ToString()); 
+        }
+
+        public string Abrir()
+        {
+            if (File.Exists(_path)) return File.ReadAllText(_path);
+            return "0";
+        }
+
+    }
+}
